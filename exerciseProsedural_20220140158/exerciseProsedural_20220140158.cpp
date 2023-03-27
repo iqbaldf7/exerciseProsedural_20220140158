4b76ee745d4df38f4f2d8f9edca198a705d06203
iqbaldf7 <ACER@LAPTOP-BIGAQTOQ>
2023-03-27 14:26:25 +0700

    Add project files.

---
+++ exerciseProsedural_20220140158/exerciseProsedural_20220140158.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a, double b) {
+	return (a + b) / 2;
+}
+
+string status(double rata) {
+	if (rata >= 70)
+		return "Lulus";
+	else
+		return "Gagal";
+}
+
+string status2(double rata, double nilM) {
+	if (rata >= 70 || nilM > 80)
+		return "Lulus";
+	else
+		return "Gagal";
+}
+
+int main() {
+	double nilM, nilB;
+	cout << "Masukkan nilai Matematika = ";
+	cin >> nilM;
+	cout << "Masukkan nilai Bahasa Inggris = ";
+	cin >> nilB;
+	cout << "Status kelulusan = " << status(rerata(nilM, nilB));
+	cout << "\nStatus kelulusan ke 2 = " << status2(rerata(nilM, nilB), nilM);
+}
+
+
+
